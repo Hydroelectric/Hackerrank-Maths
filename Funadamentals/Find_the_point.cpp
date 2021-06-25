e268526c8b3d0f8b7268834b357e04572d614135
Hydroelectric <86175777+Hydroelectric@users.noreply.github.com>
2021-06-25 22:30:24 +0530

    Update Find_the_point.cpp

--- Funadamentals/Find_the_point.cpp
+++ Funadamentals/Find_the_point.cpp
@@ -1,1 +1,54 @@
-
+#include <atomic>
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+ 
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+#define ll long long
+#define ld long double
+#define ar array
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 100001;
+void solve() {
+    pair<int,int> p;
+    pair<int,int>q;
+    cin>>p.first>>p.second;
+    cin>>q.first>>q.second;
+    int x = p.first-q.first;
+    int y = p.second-q.second;
+    cout<<q.first-(x)<<" "<<q.second-(y)<<nl; 
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int te = 1;
+    cin >> te;
+    while(te--) solve();
+}
